f9498b408f32cd575b43fbc23cdc93519618b9ad
AL Qudzzz <faiqul84@gmail.com>
2025-06-30 15:25:02 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <div class="rounde

--- src/app/(auth)/dashboard/page.tsx
+++ src/app/(auth)/dashboard/page.tsx
@@ -11,33 +11,8 @@ import { ImpactSummary } from "@/components/dashboard/user/impact-summary";
 
 export default function UserDashboardPage() {
   return (
-    <div className="grid gap-8 lg:grid-cols-3">
-      <div className="lg:col-span-2 space-y-8">
-        <Card>
-          <CardHeader>
-            <CardTitle>Schedule a New Pickup</CardTitle>
-            <CardDescription>
-              Fill out the form below to schedule a new waste pickup.
-            </CardDescription>
-          </CardHeader>
-          <CardContent>
-            <SchedulePickupForm />
-          </CardContent>
-        </Card>
-        <Card>
-          <CardHeader>
-            <CardTitle>Pickup History</CardTitle>
-            <CardDescription>
-              View the status of your recent and ongoing pickups.
-            </CardDescription>
-          </CardHeader>
-          <CardContent>
-            <PickupHistory />
-          </CardContent>
-        </Card>
-      </div>
-      <div className="lg:col-span-1">
-        <Card className="sticky top-24">
+    <div className="space-y-8">
+       <Card>
           <CardHeader>
             <CardTitle>Your Green Impact</CardTitle>
             <CardDescription>
@@ -48,7 +23,30 @@ export default function UserDashboardPage() {
             <ImpactSummary />
           </CardContent>
         </Card>
-      </div>
+
+      <Card>
+        <CardHeader>
+          <CardTitle>Schedule a New Pickup</CardTitle>
+          <CardDescription>
+            Fill out the form below to schedule a new waste pickup.
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <SchedulePickupForm />
+        </CardContent>
+      </Card>
+
+      <Card>
+        <CardHeader>
+          <CardTitle>Pickup History</CardTitle>
+          <CardDescription>
+            View the status of your recent and ongoing pickups.
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <PickupHistory />
+        </CardContent>
+      </Card>
     </div>
   );
 }
